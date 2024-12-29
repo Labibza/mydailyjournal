b27c6b16c6d1c061023bab01324f4982ff623532
Labib <111202315339@mhs.dinus.ac.id>
2024-12-30 02:10:42 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>My Daily Journal</title>
-    <link rel="icon" href="img/logo.png" />
+    <link rel="icon" href="img/Lbb.svg" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
