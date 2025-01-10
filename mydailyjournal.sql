394b5631029460571e428bf08f7f4918a526d12e
Labib <111202315339@mhs.dinus.ac.id>
2025-01-10 22:09:26 +0700

    database yang baru

--- mydailyjournal.sql
+++ mydailyjournal.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2024 at 08:19 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 10 Jan 2025 pada 16.05
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `article`
+-- Struktur dari tabel `article`
 --
 
 CREATE TABLE `article` (
@@ -37,19 +37,44 @@ CREATE TABLE `article` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `article`
+-- Dumping data untuk tabel `article`
 --
 
 INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
 (1, 'PPK Ormawa Kamadiksi', 'PPKO Kamadiksi Udinus Dorong Desa Sendang Sikucing Jadi Desa Maritim Unggul\r\n', 'Artikel1.png', '2024-11-07 21:20:33', 'Labieb'),
 (2, 'Seminar Nasional Kemahasiswaan Dan Call Of Paper #2', 'Mahasiswa Udinus Raih Penghargaan Presenter Terbaik 2 Di Seminar Nasional Kemahasiswaan Dan Call Of Paper #2\r\n', 'Artikel2.png', '2024-08-24 21:20:33', 'Labieb'),
-(3, 'Pengalaman Saya Sebagai Mahasiswa Udinus', 'Labib Buktikan Penerima Beasiswa Punya Kesempatan yang Sama untuk Berprestasi dan Aktif di Kegiatan Kemahasiswaan', 'Artikel3.png', '2024-08-30 07:06:33', 'Labieb'),
+(3, 'Pengalaman Saya Sebagai Mahasiswa Udinus', 'Saya Mahasiswa Teknik Informatika UDINUS yang Berprestasi dan Aktif di Kegiatan Kemahasiswaan', 'Artikel3.png', '2025-01-10 06:20:48', 'admin'),
 (4, 'Refreshing Kepemimpinan KIP Udinus 2024', 'Refreshing Kepemimpinan, Membangun Karakter Mahasiswa KIP Udinus Menuju Prestasi Internasional', 'Artikel4.png', '2024-12-10 16:13:48', 'Labieb');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id_gallery` int(11) NOT NULL,
+  `gambar` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `username` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `gallery`
+--
+
+INSERT INTO `gallery` (`id_gallery`, `gambar`, `tanggal`, `username`) VALUES
+(1, 'Gallery1.png', '2025-01-09 12:40:54', 'admin'),
+(2, 'Gallery2.png', '2025-01-09 12:40:54', 'admin'),
+(3, 'Gallery3.png', '2025-01-09 12:42:21', 'admin'),
+(4, 'Gallery4.png', '2025-01-09 12:42:21', 'admin'),
+(5, 'Gallery5.png', '2025-01-09 12:42:21', 'admin'),
+(6, 'Gallery6.png', '2025-01-09 12:42:21', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -60,43 +85,56 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '');
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'LBB - LOGO (1).png'),
+(2, 'labib', '0192023a7bbd73250516f069df18b500', 'lbb.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `article`
+-- Indeks untuk tabel `article`
 --
 ALTER TABLE `article`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id_gallery`);
+
+--
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `article`
+-- AUTO_INCREMENT untuk tabel `article`
 --
 ALTER TABLE `article`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id_gallery` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
